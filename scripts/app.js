be5d678922cae69594c71f853ef649d242b31d34
aquoss <amberquoss@gmail.com>
2016-12-08 19:10:27 -0800

    adds horizontal movement and initializes position with offset()

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,43 @@
-//add in document ready
+$(document).on("ready", function(){
 
-//initializes witch object
-var witch = {
-  height: 0
-};
+  //initializes witch object
+  var witch = {
+    height: 0,
+    left: $('#witch').offset().left
+  };
 
-//initial witch jump/fall with spacebar
-$(window).keypress(function (e) {
-  if (e.keyCode === 0 || e.keyCode === 32) {
-    e.preventDefault();
-    var intervalUp = setInterval(function(){
-      witch.height+=2;
-      $('#witch').css('bottom',witch.height);
-      if (witch.height>=250){
-        clearInterval(intervalUp);
-      }
-    }, 1);
-    var intervalDown = setInterval(function(){
-      witch.height-=1;
-      $('#witch').css('bottom',witch.height);
-      if (witch.height<=0){
-        clearInterval(intervalDown);
-      }
-    }, 10);
-  }
+  //initial witch jump/fall with spacebar
+  $(window).keypress(function(e){
+    if (e.keyCode === 32) {
+      e.preventDefault();
+      var intervalUp = setInterval(function(){
+        witch.height+=2;
+        $('#witch').css('bottom',witch.height);
+        if (witch.height>=250){
+          clearInterval(intervalUp);
+        }
+      }, 1);
+      var intervalDown = setInterval(function(){
+        witch.height-=1;
+        $('#witch').css('bottom',witch.height);
+        if (witch.height<=0){
+          clearInterval(intervalDown);
+        }
+      }, 10);
+    }
+  })
+
+  //find correct key codes!!!!!!!!!
+  $(window).keypress(function(e){
+    if (e.keyCode === 39){
+      e.preventDefault();
+      witch.left+=15;
+      $('#witch').css('left',witch.left);
+    } else if (e.keyCode === 37){
+      e.preventDefault();
+      witch.left-=15;
+      $('#witch').css('right',witch.left);
+    }
+  })
+
 })
