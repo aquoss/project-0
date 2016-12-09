0dd2973d0264102f9a4140f83eeb3bcfd919e6dc
aquoss <amberquoss@gmail.com>
2016-12-08 17:02:26 -0800

    adds functions for vertical witch movement

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,23 @@
+//add in document ready
+//when to stop witch from moving
+
 var witch = {
-  height: x,
-  width: x
+  height: 0
+};
+
+$(window).keypress(function (e) {
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault();
+    setInterval(moveWitchUp, 1);
+  }
+})
+
+//functions to move the witch up or down
+function moveWitchUp(){
+  witch.height+=2;
+  $('#witch').css('bottom',witch.height);
+}
+function moveWitchDown(){
+  witch.height-=1;
+  $('#witch').css('bottom',witch.height);
 }
