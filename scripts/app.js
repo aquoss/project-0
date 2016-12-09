909001af3775e2c0255ee3e402424329da9b6029
aquoss <amberquoss@gmail.com>
2016-12-08 17:55:51 -0800

    adds decreasing height to witch after reaching max height

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,27 @@
 //add in document ready
-//when to stop witch from moving
 
+//initializes witch object
 var witch = {
   height: 0
 };
 
+//initial witch jump/fall with spacebar
 $(window).keypress(function (e) {
   if (e.keyCode === 0 || e.keyCode === 32) {
     e.preventDefault();
-    setInterval(moveWitchUp, 1);
+    var intervalUp = setInterval(function(){
+      witch.height+=2;
+      $('#witch').css('bottom',witch.height);
+      if (witch.height>=250){
+        clearInterval(intervalUp);
+      }
+    }, 1);
+    var intervalDown = setInterval(function(){
+      witch.height-=1;
+      $('#witch').css('bottom',witch.height);
+      if (witch.height<=0){
+        clearInterval(intervalDown);
+      }
+    }, 10);
   }
 })
-
-//functions to move the witch up or down
-function moveWitchUp(){
-  witch.height+=2;
-  $('#witch').css('bottom',witch.height);
-}
-function moveWitchDown(){
-  witch.height-=1;
-  $('#witch').css('bottom',witch.height);
-}
